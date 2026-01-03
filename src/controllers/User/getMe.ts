bdd5c9c330881dbd01bb1a847ade3c998dd6061f
salah-72 <a7med11salah@gmail.com>
2026-01-03 16:21:14 +0200

    fix: get my profile

--- src/controllers/User/getMe.ts
+++ src/controllers/User/getMe.ts
@@ -1,3 +1,4 @@
+import Post from '@/models/postModel';
 import User from '@/models/userModel';
 import appError from '@/utils/appError';
 import catchAsync from '@/utils/catchAsync';
@@ -5,12 +6,26 @@ import { Request, Response, NextFunction } from 'express';
 
 export const getMe = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const user = await User.findOne({ email: req.currentuser?.email });
-    if (!user) return next(new appError('your account is not found', 404));
+    const user = await User.findById(req.currentuser?._id)
+      .select(
+        '-password -emailVerificationToken -passwordResetToken -passwordResetExpires -__v -emailVerified -active',
+      )
+      .lean();
+
+    const posts = await Post.find({
+      author: req.targetUser?._id,
+      status: 'published',
+    })
+      .select('-_id')
+      .lean();
 
     res.status(200).json({
       status: 'success',
-      userInfo: user,
+      data: {
+        userInfo: user,
+        postsCount: posts!.length,
+        posts,
+      },
     });
   },
 );
