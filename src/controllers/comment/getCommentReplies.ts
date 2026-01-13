72b96b073316ec0b7f2aa354e0195611b9755809
salah-72 <a7med11salah@gmail.com>
2026-01-13 14:00:25 +0200

    fix(comment): filter deleted users when getting replies with aggregation

--- src/controllers/comment/getCommentReplies.ts
+++ src/controllers/comment/getCommentReplies.ts
@@ -1,8 +1,10 @@
 import Block from '@/models/blockModel';
 import Comment from '@/models/commentModel';
+import User from '@/models/userModel';
 import appError from '@/utils/appError';
 import catchAsync from '@/utils/catchAsync';
 import { Request, Response, NextFunction } from 'express';
+import { Types } from 'mongoose';
 
 export const commentReplies = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -33,17 +35,52 @@ export const commentReplies = catchAsync(
     )
       return next(new appError('comment not found', 404));
 
-    const replies = await Comment.find({
-      post: postId,
-      user: { $nin: blocksIds },
-      parentComment: commentId,
-    })
-      .select('-post -parentComment -_id -__v')
-      .populate('user', 'username profilePhoto firstName lastName')
-      .sort('createdAt')
-      .skip(skip)
-      .limit(limit)
-      .lean();
+    const userExist = await User.exists({
+      _id: comment.user,
+      active: true,
+    });
+    if (!userExist) return next(new appError('comment not found', 404));
+
+    const replies = await Comment.aggregate([
+      {
+        $match: {
+          post: new Types.ObjectId(postId),
+          user: { $nin: blocksIds },
+          parentComment: new Types.ObjectId(commentId),
+        },
+      },
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'user',
+          foreignField: '_id',
+          as: 'user',
+        },
+      },
+      {
+        $unwind: '$user',
+      },
+      {
+        $match: {
+          'user.active': true,
+        },
+      },
+      {
+        $project: {
+          'user.username': 1,
+          'user.profilePhoto': 1,
+          'user.firstName': 1,
+          'user.lastName': 1,
+          _id: 0,
+          content: 1,
+          createdAt: 1,
+          updatedAt: 1,
+        },
+      },
+      { $sort: { createdAt: 1 } },
+      { $skip: skip },
+      { $limit: limit },
+    ]);
 
     res.status(200).json({
       status: 'success',
