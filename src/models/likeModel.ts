0f0bdc65832f0c54c88b80ac9447e56e763e6282
salah-72 <a7med11salah@gmail.com>
2026-01-17 11:29:13 +0200

    feat(like): extend like schema to handle comment and story

--- src/models/likeModel.ts
+++ src/models/likeModel.ts
@@ -1,8 +1,15 @@
-import { model, Schema, Types } from 'mongoose';
+import {
+  CallbackWithoutResultAndOptionalError,
+  model,
+  Schema,
+  Types,
+} from 'mongoose';
 
 export interface ILike {
   user: Types.ObjectId;
-  post: Types.ObjectId;
+  post?: Types.ObjectId;
+  comment?: Types.ObjectId;
+  story?: Types.ObjectId;
   type: 'like' | 'love' | 'care' | 'sad' | 'angry' | 'haha' | 'wow';
 }
 
@@ -16,7 +23,14 @@ const likeSchema = new Schema<ILike>(
     post: {
       type: Types.ObjectId,
       ref: 'Post',
-      required: [true, 'post is required'],
+    },
+    comment: {
+      type: Types.ObjectId,
+      ref: 'Comment',
+    },
+    story: {
+      type: Types.ObjectId,
+      ref: 'Story',
     },
     type: {
       type: String,
@@ -28,6 +42,19 @@ const likeSchema = new Schema<ILike>(
 );
 
 likeSchema.index({ user: 1, createdAt: -1 });
+
+likeSchema.pre(
+  'validate' as any,
+  function (next: CallbackWithoutResultAndOptionalError) {
+    const fields = [this.post, this.comment, this.story].filter(Boolean);
+    if (fields.length === 0)
+      return next(new Error('like must contain post or comment or story'));
+    if (fields.length > 0)
+      return next(new Error('like must contain only one field'));
+
+    next();
+  },
+);
 
 const Like = model<ILike>('Like', likeSchema);
 export default Like;
