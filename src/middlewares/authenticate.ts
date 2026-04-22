3e55e39e671ce409b5026b79df271e0b1c128c22
salah-72 <a7med11salah@gmail.com>
2026-04-22 13:58:06 +0200

    fix(auth): authenticate middleware

--- src/middlewares/authenticate.ts
+++ src/middlewares/authenticate.ts
@@ -5,6 +5,7 @@ import catchAsync from '@/utils/catchAsync';
 import { Request, Response, NextFunction } from 'express';
 import { Types } from 'mongoose';
 import jwt from 'jsonwebtoken';
+import BlackList from '@/models/blackListTokensModel';
 
 interface JwtRefreshPayload extends jwt.JwtPayload {
   userId: Types.ObjectId;
@@ -18,12 +19,24 @@ export const authenticate = catchAsync(
 
     const token = auth.split(' ')[1];
 
+    const isBlacklisted = await BlackList.exists({ token });
+
+    if (isBlacklisted)
+      return next(
+        new appError('Token has been revoked. Please login again.', 401),
+      );
+
     const payload = jwt.verify(
       token,
       config.JWT_ACCESS_KEY,
     ) as JwtRefreshPayload;
 
     const user = await User.findById(payload._id);
+
+    if (!user)
+      return next(
+        new appError('The user belonging to this token no longer exists', 401),
+      );
 
     if (!user?.emailVerified)
       return next(
