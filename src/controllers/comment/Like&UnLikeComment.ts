e1877b0d667df1962570862cc8b19c256af9fe03
salah-72 <a7med11salah@gmail.com>
2026-07-19 15:51:03 +0300

    feat: add notification updates for comment liking and unliking toggles

--- src/controllers/comment/Like&UnLikeComment.ts
+++ src/controllers/comment/Like&UnLikeComment.ts
@@ -1,9 +1,11 @@
 import { incrementLike, decrementLike } from '@/functions/likeCounter';
-import Block from '@/models/blockModel';
 import Comment from '@/models/commentModel';
 import Like from '@/models/likeModel';
+import Notification from '@/models/notificationModel';
 import appError from '@/utils/appError';
+import redisClient from '@/utils/redis';
 import catchAsync from '@/utils/catchAsync';
+import { sendNotification } from '@/utils/sendNotification';
 import { sendResponse } from '@/utils/sendResponse';
 import { Request, Response, NextFunction } from 'express';
 
@@ -27,17 +29,43 @@ export const likeComment = catchAsync(
         type,
       });
 
-      await incrementLike('comment', commentId);
+      await Promise.all([
+        incrementLike('comment', commentId),
+        sendNotification({
+          recipient: comment.user,
+          sender: req.currentuser!._id,
+          comment: commentId,
+          type: 'comment_like',
+        }),
+      ]);
 
       return sendResponse(res, 201, undefined, { message: 'comment liked' });
     } catch (err: any) {
       if (err.code === 11000) {
-        await Like.deleteOne({
-          user: req.currentuser?._id,
+        const notification = await Notification.findOneAndDelete({
+          recipient: comment.user,
+          sender: req.currentuser!._id,
           comment: commentId,
+          type: 'comment_like',
         });
 
-        await decrementLike('comment', commentId);
+        if (notification && !notification.isRead) {
+          const currentCount = await redisClient.get(
+            `user:unread_notifications:${comment.user}`,
+          );
+          if (currentCount && parseInt(currentCount) > 0) {
+            await redisClient.decr(`user:unread_notifications:${comment.user}`);
+          }
+        }
+
+        await Promise.all([
+          Like.deleteOne({
+            user: req.currentuser?._id,
+            comment: commentId,
+          }),
+
+          decrementLike('comment', commentId),
+        ]);
 
         return res.status(204).send();
       }
