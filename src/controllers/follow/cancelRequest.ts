1d0fac2a8016ebd35b5034bf8dceaa9e4f6ee418
salah-72 <a7med11salah@gmail.com>
2026-07-22 14:05:22 +0300

    feat(follows): add notification deletion and counter update when cancelling follow requests

--- src/controllers/follow/cancelRequest.ts
+++ src/controllers/follow/cancelRequest.ts
@@ -1,7 +1,10 @@
 import { logger } from '@/lib/winston';
 import Follow from '@/models/followModel';
+import Notification from '@/models/notificationModel';
+import { removeRealtimeNotification } from '@/socket';
 import appError from '@/utils/appError';
 import catchAsync from '@/utils/catchAsync';
+import redisClient from '@/utils/redis';
 import { Request, Response, NextFunction } from 'express';
 
 export const cancelReq = catchAsync(
@@ -20,7 +23,22 @@ export const cancelReq = catchAsync(
       );
 
     await Follow.deleteOne({ _id: follow._id });
-
+    const notification = await Notification.findOneAndDelete({
+      recipient: following,
+      sender: follower,
+      type: 'follow_request',
+    });
+    if (notification) {
+      await removeRealtimeNotification(following, notification._id.toString());
+      if (!notification.isRead) {
+        const currentCount = await redisClient.get(
+          `user:unread_notifications:${following}`,
+        );
+        if (currentCount && parseInt(currentCount) > 0) {
+          await redisClient.decr(`user:unread_notifications:${following}`);
+        }
+      }
+    }
     logger.warn(`${follower} canceled follow request ${following}`);
 
     res.status(204).send();
