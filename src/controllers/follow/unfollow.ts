6a6a37ededdcd24b658d722df49a2e8bd237710f
salah-72 <a7med11salah@gmail.com>
2026-07-22 16:37:39 +0300

    feat(follows): add unfollow logic with counter updates and notification deletion

--- src/controllers/follow/unfollow.ts
+++ src/controllers/follow/unfollow.ts
@@ -3,6 +3,7 @@ import Follow from '@/models/followModel';
 import User from '@/models/userModel';
 import appError from '@/utils/appError';
 import catchAsync from '@/utils/catchAsync';
+import { deleteNotification } from '@/utils/deleteNotification';
 import { Request, Response, NextFunction } from 'express';
 
 export const unfollow = catchAsync(
@@ -18,13 +19,15 @@ export const unfollow = catchAsync(
     if (!follow)
       return next(new appError(`you are not following this user`, 400));
 
-    // await Follow.deleteOne({ _id: follow._id });
-    // await User.findByIdAndUpdate(follower, { $inc: { following: -1 } });
-    // await User.findByIdAndUpdate(following, { $inc: { followers: -1 } });
     await Promise.all([
       Follow.deleteOne({ _id: follow._id }),
-      User.findByIdAndUpdate(follower, { $inc: { following: -1 } }),
-      User.findByIdAndUpdate(following, { $inc: { followers: -1 } }),
+      User.updateOne({ _id: follower }, { $inc: { following: -1 } }),
+      User.updateOne({ _id: following }, { $inc: { followers: -1 } }),
+      deleteNotification({
+        recipient: following!,
+        sender: follower!,
+        type: 'follow',
+      }),
     ]);
 
     logger.warn(`${follower} stop following ${following}`);
