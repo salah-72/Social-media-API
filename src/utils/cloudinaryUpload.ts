d04aa74f1a8625050fe58aff351bd942cfafa8cd
salah-72 <a7med11salah@gmail.com>
2026-09-17 18:52:56 +0300

    feat(upload): enhance uploadToCloudinary to support video uploads

--- src/utils/cloudinaryUpload.ts
+++ src/utils/cloudinaryUpload.ts
@@ -1,18 +1,29 @@
 import cloudinary from '@/config/cloudinaryConfig';
 
+interface UploadResults {
+  secure_url: string;
+  public_id: string;
+  duration?: number;
+}
+
 export const uploadToCloudinary = (
   buffer: Buffer,
   folder: string,
-): Promise<{ secure_url: string; public_id: string }> => {
+  resourceType: 'image' | 'video' = 'image',
+): Promise<UploadResults> => {
   return new Promise((resolve, reject) => {
     cloudinary.uploader
-      .upload_stream({ folder }, (error, result) => {
-        if (error || !result) return reject(error);
-        resolve({
-          secure_url: result.secure_url,
-          public_id: result.public_id,
-        });
-      })
+      .upload_stream(
+        { folder, resource_type: resourceType },
+        (error, result) => {
+          if (error || !result) return reject(error);
+          resolve({
+            secure_url: result.secure_url,
+            public_id: result.public_id,
+            duration: result.duration,
+          });
+        },
+      )
       .end(buffer);
   });
 };
